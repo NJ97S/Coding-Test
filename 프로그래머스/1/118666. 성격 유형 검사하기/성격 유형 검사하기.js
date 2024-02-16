3220c5629f64d0ba17164108e080fe1d38cb752a
Namju So <79499733+NJ97S@users.noreply.github.com>
2024-02-17 03:10:44 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.75 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -0,0 +1,44 @@
+function solution(survey, choices) {
+    let answer = '';
+    
+    const MBTI = ['RT', 'CF', 'JM', 'AN']
+    let points = [
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0]
+    ]
+    
+    for (let i = 0; i < survey.length; i++) {
+        if (MBTI.includes(survey[i])) {
+            let idx = MBTI.indexOf(survey[i]);
+            if (choices[i] < 4) {
+                points[idx][0] += 4 - choices[i]
+            } else if (choices[i] > 4) {
+                points[idx][1] += choices[i] - 4
+            }
+        } else {
+            let reverseSurvey = survey[i].split('').reverse().join('')
+            let idx = MBTI.indexOf(reverseSurvey);
+            if (choices[i] < 4) {
+                points[idx][1] += 4 - choices[i]
+            } else if (choices[i] > 4) {
+                points[idx][0] += choices[i] - 4
+            }
+        }
+    }
+    
+    for (let i = 0; i < 4; i++) {
+        let type = MBTI[i].split('')
+        if (points[i][0] > points[i][1]) {
+            answer += type[0]
+        } else if (points[i][0] < points[i][1]) {
+            answer += type[1]
+        } else {
+            type.sort()
+            answer += type[0]
+        }
+    }
+    
+    return answer;
+}

2d062198a2cdbf3d2cfce060e60d3b7815e02073
Namju So <79499733+NJ97S@users.noreply.github.com>
2024-02-26 02:23:56 +0900

    [Silver IV] Title: 바닥 장식, Time: 132 ms, Memory: 9568 KB -BaekjoonHub

---
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.js
@@ -0,0 +1,39 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [first, ...floor] = input;
+const [N, M] = first.split(" ");
+
+let Array = [];
+for (let i = 0; i < floor.length; i++) {
+  Array[i] = [];
+  let tempArray = floor[i].split("");
+  if (tempArray[tempArray.length - 1] === "\r") tempArray.pop();
+
+  for (let elem of tempArray) {
+    if (elem === "-") Array[i].push(0);
+    else Array[i].push(1);
+  }
+}
+
+let count = 0;
+function countBoard(Array) {
+  for (let j = 0; j < M; j++) {
+    if (Array[0][j] === 0 && Array[0][j] === Array[0][j - 1]) count += 0;
+    else count++;
+  }
+
+  for (let i = 1; i < N; i++) {
+    for (let j = 0; j < M; j++) {
+      if (Array[i][j] === 0 && Array[i][j] === Array[i][j - 1]) count += 0;
+      else if (Array[i][j] === 1 && Array[i][j] === Array[i - 1][j]) count += 0;
+      else count++;
+    }
+  }
+  console.log(count);
+}
+
+countBoard(Array);

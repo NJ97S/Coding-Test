9b360a86eb720041ce5dccaaffa3f8e97cfa55b4
Namju So <79499733+NJ97S@users.noreply.github.com>
2024-02-26 02:28:22 +0900

    [Silver IV] Title: 바닥 장식, Time: 120 ms, Memory: 9568 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.js
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.js
@@ -7,6 +7,7 @@ const input = require("fs")
 const [first, ...floor] = input;
 const [N, M] = first.split(" ");
 
+// 0과 1로 이루어진 행렬로 표현
 let Array = [];
 for (let i = 0; i < floor.length; i++) {
   Array[i] = [];
@@ -19,17 +20,18 @@ for (let i = 0; i < floor.length; i++) {
   }
 }
 
+// 왼쪽 요소와 같지 않은 0이거나, 위쪽 요소와 같지 않은 1일 경우, count++
 let count = 0;
 function countBoard(Array) {
   for (let j = 0; j < M; j++) {
-    if (Array[0][j] === 0 && Array[0][j] === Array[0][j - 1]) count += 0;
+    if (Array[0][j] === 0 && Array[0][j] === Array[0][j - 1]) continue;
     else count++;
   }
 
   for (let i = 1; i < N; i++) {
     for (let j = 0; j < M; j++) {
-      if (Array[i][j] === 0 && Array[i][j] === Array[i][j - 1]) count += 0;
-      else if (Array[i][j] === 1 && Array[i][j] === Array[i - 1][j]) count += 0;
+      if (Array[i][j] === 0 && Array[i][j] === Array[i][j - 1]) continue;
+      else if (Array[i][j] === 1 && Array[i][j] === Array[i - 1][j]) continue;
       else count++;
     }
   }

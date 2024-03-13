1008911aacd06d63d339c64719c19dea1a13b245
Namju So <79499733+NJ97S@users.noreply.github.com>
2024-03-13 15:08:10 +0900

    [level 1] Title: 모의고사, Time: 3.10 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,22 @@
+function solution(answers) {
+    const GG = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+        
+    let points = [0, 0, 0]
+    
+    for (let i = 0; i < answers.length; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (answers[i] === GG[j][i % GG[j].length]) {
+                points[j] ++
+            }
+        }
+    }
+    
+    let answer = [];
+    for (let i = 0; i < points.length; i++) {
+        if (points[i] === Math.max(points[0], points[1], points[2])) {
+            answer.push(i + 1)
+        }
+    }
+    return answer
+
+}

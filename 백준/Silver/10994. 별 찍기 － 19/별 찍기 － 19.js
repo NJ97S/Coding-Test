ae5374d86923c2b80ac927982230727ce18356d7
Namju So <79499733+NJ97S@users.noreply.github.com>
2024-03-28 02:48:51 +0900

    [Silver IV] Title: 별 찍기 - 19, Time: 228 ms, Memory: 13840 KB -BaekjoonHub

--- 백준/Silver/10994. 별 찍기 － 19/별 찍기 － 19.js
+++ 백준/Silver/10994. 별 찍기 － 19/별 찍기 － 19.js
@@ -8,18 +8,15 @@ const WIDTH = 4 * N - 3;
 let square = Array.from({ length: WIDTH }, () => Array(WIDTH).fill("*"));
 
 for (let i = 1; i < WIDTH; i += 2) {
-  let innerWidth = WIDTH - 2 * i;
   let start = i;
-  let end = i + innerWidth - 1;
+  let end = i + (WIDTH - 2 * i) - 1;
 
   for (let x = start; x <= end; x++) {
     square[start][x] = " ";
-    square[end][x] = " ";
-  }
+    square[x][start] = " ";
 
-  for (let y = start; y <= end; y++) {
-    square[y][start] = " ";
-    square[y][end] = " ";
+    square[end][x] = " ";
+    square[x][end] = " ";
   }
 }
 

d810cf002c798f74c893465d444a905a1f3bb248
Namju So <79499733+NJ97S@users.noreply.github.com>
2024-08-08 14:20:57 +0900

    [D2] Title: 숫자를 정렬하자, Time: 183 ms, Memory: 22,608 KB -BaekjoonHub

--- SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.java
+++ SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.java
@@ -1,53 +1,39 @@
 import java.util.Scanner;
 
 public class Solution {
-
-  public static void main(String[] args) {
-    
-    Scanner sc = new Scanner(System.in);
-
-    final int T = sc.nextInt();
-
-    for (int testCase = 1; testCase <= T; testCase++) {
-      final int N = sc.nextInt();
-
-      int[] INPUT = new int[N];
-      for (int i = 0; i < N; i++) {
-        INPUT[i] = sc.nextInt();
-      }
-
-      // 카운팅 정렬
-      int maxNum = INPUT[0];
-      for (int i = 1; i < INPUT.length; i++) {
-        maxNum = Math.max(maxNum, INPUT[i]);
-      }
-
-      int[] count = new int[maxNum + 1];
-      for (int i = 0; i < INPUT.length; i++) {
-        count[INPUT[i]]++;
-      }
-
-      for (int i = 1; i < count.length; i++) {
-        count[i] += count[i - 1];
-      }
-
-      int[] sortedArr = new int[INPUT.length];
-      for (int i = INPUT.length - 1; i >= 0; i--) {
-        sortedArr[count[INPUT[i]] - 1] = INPUT[i];
-        count[INPUT[i]]--;
-      }
-
-      // 출력
-      System.out.print("#" + testCase + " ");
-
-      String caseAnswer = "";
-      for (int elem: sortedArr) caseAnswer += elem + " ";
-      
-      System.out.println(caseAnswer.trim());
+	
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        final int T = sc.nextInt();
+        
+        for (int testCase = 1; testCase <= T; testCase++) {
+        	final int N = sc.nextInt();
+        	
+        	int[] INPUT = new int[N];
+        	for (int i = 0; i < N; i++) {
+        		INPUT[i] = sc.nextInt();
+        	}
+        	
+        	for (int i = 1; i < INPUT.length; i++) {
+        		int curData = INPUT[i];
+        		
+        		int j;
+        		for (j = i - 1; j >= 0 && INPUT[j] > curData; j--) {
+        			INPUT[j + 1] = INPUT[j];
+        		}
+        		
+        		INPUT[j + 1] = curData;
+        	}
+        	
+        	System.out.printf("#%d", testCase);
+        	
+        	for (int elem: INPUT) System.out.printf(" %d", elem);
+        	
+        	System.out.println("");
+        }
+        
+        sc.close();
     }
-
-    sc.close();
-
-  }
-
+    
 }

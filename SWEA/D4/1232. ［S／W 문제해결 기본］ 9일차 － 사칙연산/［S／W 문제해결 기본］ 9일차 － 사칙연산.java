00967ca655a2b745d0a20df0df769f430bfdd043
Namju So <79499733+NJ97S@users.noreply.github.com>
2024-08-22 19:44:49 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산, Time: 129 ms, Memory: 32,896 KB -BaekjoonHub

--- SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.java
+++ SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.java
@@ -4,87 +4,84 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Solution {
-    
-    static int[][] tree;
-    static String[] value;
-    static StringBuilder sb;
-    static Stack<Integer> stack;
-    
-    public static void main(String[] args) throws IOException {
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuilder();
-        
-        for (int testCase = 1; testCase <= 10; testCase++) {
-            int N = Integer.valueOf(br.readLine());
-            
-            tree = new int[N + 1][2];
-            value = new String[N + 1];
-            
-            stack = new Stack<>();
-            
-            for (int i = 1; i <= N; i++) {
-                String[] line = br.readLine().split(" ");
-                
-                value[i] = line[1];
-                
-                if (line.length == 4) {
-                    int a = Integer.valueOf(line[0]);
-                    int b = Integer.valueOf(line[2]);
-                    int c = Integer.valueOf(line[3]);
-                    
-                    tree[a][0] = b;
-                    tree[a][1] = c;
-                }
-            }
-            
-            postOrder(1);
-            
-            int answer = stack.pop();
-            
-            System.out.printf("#%d %d\n", testCase, answer);
-        }
-        
-    }
-    
-    static void postOrder(int root) {
-        // 자식 노드가 없을 때
-        if (tree[root][0] == 0 && tree[root][1] == 0) {
-            operation(value[root]);
-            return;
-        }
-        // 자식 노드가 있을 때
-    	if (tree[root][0] != 0) postOrder(tree[root][0]);
-    	if (tree[root][1] != 0) postOrder(tree[root][1]);
-        
-        operation(value[root]);
-    }
-    
-    static void operation(String elem) {
-    	// 숫자일 경우
-    	if (!elem.equals("+") && !elem.equals("-") && !elem.equals("*") && !elem.equals("/")) {
-    		stack.push(Integer.valueOf(elem));
-    		return;
-    	}
-    	
-    	// 연산자일 경우
-    	int num2 = stack.pop();
-    	int num1 = stack.pop();
-    	
-    	switch(elem) {
-    	case "+":
-    		stack.push(num1 + num2);
-    		break;
-    	case "-":
-    		stack.push(num1 - num2);
-    		break;
-    	case "*":
-    		stack.push(num1 * num2);
-    		break;
-    	case "/":
-    		stack.push(num1 / num2);
-    		break;
-    	}
-    }
-    
+	
+	static BufferedReader br;
+	static StringBuilder sb;
+	
+	static int N; // 정점의 개수 (1 <= N <= 1,000)
+	
+	static String[] value; // 정점의 값
+	static int[][] linkedList; // 연결 리스트
+	
+	static Stack<Double> stack; // 숫자 저장해놓을 스택
+	
+	public static void main(String[] args) throws IOException {
+		
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		
+		for (int testCase = 1; testCase <= 10; testCase++) {
+			sb.append("#").append(testCase).append(" ");
+			
+			N = Integer.parseInt(br.readLine());
+			
+			value = new String[N + 1];
+			linkedList = new int[N + 1][2];
+			
+			for (int i = 1; i <= N; i++) {
+				String[] input = br.readLine().split(" ");
+				
+				value[i] = input[1];
+				
+				if (input.length == 4) { // 자식 노드 있을 떄					
+					linkedList[i][0] = Integer.parseInt(input[2]);
+					linkedList[i][1] = Integer.parseInt(input[3]);
+				}
+			}
+			
+			stack = new Stack<>();
+			
+			postOrder(1);
+			
+			sb.append((int) Math.floor(stack.pop())).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+		
+	}
+	
+	static void postOrder(int root) {
+		if (root < 1) return;
+		
+		if (linkedList[root][0] > 0 && linkedList[root][0] <= N) postOrder(linkedList[root][0]);
+		if (linkedList[root][1] > 0 && linkedList[root][1] <= N) postOrder(linkedList[root][1]);
+		
+		calculate(value[root]);
+	}
+	
+	static void calculate(String value) {
+		if (!value.equals("+") && !value.equals("-") && !value.equals("*") && !value.equals("/")) {
+			stack.push((double) Integer.parseInt(value));
+			return;
+		}
+		
+		double num2 = stack.pop();
+		double num1 = stack.pop();
+		
+		switch(value) {
+		case "+":
+			stack.push(num1 + num2);
+			break;
+		case "-":
+			stack.push(num1 - num2);
+			break;
+		case "*":
+			stack.push(num1 * num2);
+			break;
+		case "/":
+			stack.push(num1 / num2);
+			break;
+		}
+	}
+	
 }

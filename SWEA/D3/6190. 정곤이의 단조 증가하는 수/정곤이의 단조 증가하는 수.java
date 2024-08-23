5d1c9a11456c08c2eebb66859435511a5f4ad022
Namju So <79499733+NJ97S@users.noreply.github.com>
2024-08-23 15:22:49 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 310 ms, Memory: 28,680 KB -BaekjoonHub

--- SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.java
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.java
@@ -7,8 +7,8 @@ public class Solution {
 	static BufferedReader br;
 	static StringBuilder sb;
 	
-	static int N; // 양의 정수의 수
-	static int[] NUMBERS; // 양의 정수 배열
+	static int N; // 양의 정수의 수 (1 <= N <= 1,000)
+	static int[] INPUT; // (1 <= 각 숫자 <= 30,000)
 	
 	static int answer; // 단조 증가하는 수 중 최댓값
 	
@@ -24,40 +24,40 @@ public class Solution {
 			
 			N = Integer.parseInt(br.readLine());
 			
-			String[] input = br.readLine().split(" ");
+			INPUT = new int[N];
 			
-			NUMBERS = new int[N];
+			String[] line = br.readLine().split(" ");
 			for (int i = 0; i < N; i++) {
-				NUMBERS[i] = Integer.parseInt(input[i]);
+				INPUT[i] = Integer.parseInt(line[i]);
 			}
 			
-			answer = 0;
+			answer = -1;
 			
 			for (int i = 0; i < N; i++) {
 				for (int j = i + 1; j < N; j++) {
-					int temp = NUMBERS[i] * NUMBERS[j];
+					int multiply = INPUT[i] * INPUT[j];
 					
-					if (isIncreasing(temp)) answer = Math.max(answer, temp);
+					if (isIncreasing(multiply)) answer = Math.max(answer, multiply);
 				}
 			}
 			
-			sb.append(answer == 0 ? -1 : answer).append("\n");
+			sb.append(answer).append("\n");
 		}
 		
-		System.out.println(sb.toString());
+		System.out.println(sb);
 		
 	}
 	
 	static boolean isIncreasing(int num) {
-		if (num < 10) return true; // 한 자리 수라면 true 반환하고 종료
+		if (num < 10) return true;
 		
-		int lastNum = num % 10;
+		int lastElem = num % 10;
 		num /= 10;
 		
 		while (num > 0) {
-			if (lastNum < num % 10) return false;
+			if (lastElem < num % 10) return false;
 			
-			lastNum = num % 10;
+			lastElem = num % 10;
 			num /= 10;
 		}
 		

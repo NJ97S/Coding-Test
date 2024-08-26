06ab2acbc6e8373cc5ce3964ae1b5da4bdee6f42
Namju So <79499733+NJ97S@users.noreply.github.com>
2024-08-26 19:41:12 +0900

    [D3] Title: 이진수 표현, Time: 157 ms, Memory: 33,276 KB -BaekjoonHub

---
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+	
+	static BufferedReader br;
+	static StringBuilder sb;
+	
+	static int N; // 마지막 N 비트
+	static int M; // 10진수
+	
+	static String answer;
+
+	public static void main(String[] args) throws IOException {
+		
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		
+		final int T = Integer.valueOf(br.readLine());
+		
+		for (int testCase = 1; testCase <= T; testCase++) {
+			sb.append("#").append(testCase).append(" ");
+			
+			String[] input = br.readLine().split(" ");
+			N = Integer.parseInt(input[0]);
+			M = Integer.parseInt(input[1]);
+			
+			if (M == 0) {
+				sb.append("OFF").append("\n");
+				continue;
+			}
+			
+			answer = "ON";
+			
+			int repeat = 0;
+			while (M > 0 && repeat < N) {
+				repeat++;
+				
+				if (M % 2 != 1) {
+					answer = "OFF";
+					break;
+				}
+				
+				M /= 2;
+			}
+			
+			if (M == 0 && repeat < N) {
+				sb.append("OFF").append("\n");
+				continue;
+			}
+			
+			sb.append(answer).append("\n");
+		}
+		
+		System.out.println(sb);
+
+	}
+
+}

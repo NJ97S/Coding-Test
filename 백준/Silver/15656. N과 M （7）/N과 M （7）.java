457962a9ac99ce4ada062dc9c1ee154e1a7b099d
Namju So <79499733+NJ97S@users.noreply.github.com>
2024-09-14 18:35:45 +0900

    [Silver III] Title: N과 M (7), Time: 552 ms, Memory: 220604 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	
+	static BufferedReader br;
+	static StringBuilder sb;
+	
+	static int N; // 1 <= M <= N <= 7
+	static int M;
+	
+	static int[] NUMBERS;
+	
+	static int[] selected;
+	
+	public static void main(String[] args) throws IOException {
+		
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		
+		String[] input = br.readLine().split(" ");
+		N = Integer.parseInt(input[0]);
+		M = Integer.parseInt(input[1]);
+		
+		NUMBERS = new int[N];
+		
+		input = br.readLine().split(" ");
+		for (int i = 0; i < N; i++) {
+			NUMBERS[i] = Integer.parseInt(input[i]);
+		}
+		
+		// ----------------------- input -----------------------
+
+		Arrays.sort(NUMBERS);
+		
+		selected = new int[M];
+		perm(0);
+		
+		System.out.println(sb);
+		
+	}
+	
+	static void perm(int idx) {
+		
+		// 기저 조건
+		if (idx == M) {
+			for (int num: selected) sb.append(num).append(" ");
+			sb.append("\n");
+			
+			return;
+		}
+		
+		// 재귀 부분
+		for (int i = 0; i < N; i++) {
+			
+			selected[idx] = NUMBERS[i];
+			perm(idx + 1);
+			
+		}
+		
+	}
+	
+}

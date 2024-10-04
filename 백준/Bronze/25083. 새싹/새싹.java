5d0831a871b2290e02608e4dedbd4ee209095305
Namju So <79499733+NJ97S@users.noreply.github.com>
2024-10-05 00:58:58 +0900

    [Bronze V] Title: 새싹, Time: 60 ms, Memory: 11320 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,20 @@
+public class Main {
+
+	static StringBuilder sb;
+	
+	public static void main(String[] args) {
+		
+		sb = new StringBuilder();
+		
+		sb.append("         ,r'\"7\n");
+		sb.append("r`-_   ,'  ,/\n");
+		sb.append(" \\. \". L_r'\n");
+		sb.append("   `~\\/\n");
+		sb.append("      |\n");
+		sb.append("      |\n");
+		
+		System.out.println(sb);
+		
+	}
+	
+}

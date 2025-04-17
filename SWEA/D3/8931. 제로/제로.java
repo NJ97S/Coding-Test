cdf5cfa6217a1f49ea418721097ad380448eff4c
Namju So <79499733+NJ97S@users.noreply.github.com>
2025-04-18 01:25:21 +0900

    [D3] Title: 제로, Time: 600 ms, Memory: 106,136 KB -BaekjoonHub

--- SWEA/D3/8931. 제로/제로.java
+++ SWEA/D3/8931. 제로/제로.java
@@ -1,38 +1,44 @@
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
+import java.io.*;
 
 public class Solution {
+    
+    static BufferedReader br;
+    static StringBuilder sb;
+    
+    static int T;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
-		Scanner sc = new Scanner(System.in);
-		
-		final int TC = sc.nextInt();
-		
-		for (int testCase = 1; testCase <= TC; testCase++) {
-			final int K = sc.nextInt();
-			
-			 Stack<Integer> stack = new Stack<>();
-			 
-			 for (int i = 0; i < K; i++) {
-				 int input = sc.nextInt();
-				 
-				 if (input == 0) {
-					 stack.pop();
-					 continue;
-				 }
-				 
-				 stack.push(input);
-			 }
-			 
-			 int answer = 0;
-			 
-			 for (int elem: stack) answer += elem;
-			 
-			 System.out.printf("#%d %d\n", testCase, answer);
-		}
-		
-		sc.close();
+		br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        
+        T = Integer.parseInt(br.readLine());
+        
+        for (int testCase = 1; testCase <= T; testCase++) {
+            sb.append("#").append(testCase).append(" ");
+            
+        	int K = Integer.parseInt(br.readLine());
+            
+            Stack<Integer> stack = new Stack<>();
+            
+            for (int i = 0; i < K; i++) {
+            	int number = Integer.parseInt(br.readLine());
+                
+                if (number == 0) stack.pop();
+                else stack.push(number);
+            }
+            
+            int sum = 0;
+            
+            for (int elem: stack) {
+            	sum += elem;
+            }
+            
+            sb.append(sum).append("\n");
+        }
+        
+        System.out.println(sb);
 		
 	}
 

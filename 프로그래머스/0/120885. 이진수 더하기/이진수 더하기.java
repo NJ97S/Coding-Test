cfb27c1dac99c2420c877cc48a3080b00637fdb7
Namju So <79499733+NJ97S@users.noreply.github.com>
2025-11-03 23:29:13 +0900

    [level 0] Title: 이진수 더하기, Time: 0.06 ms, Memory: 88.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String bin1, String bin2) {        
+        int temp = Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2);
+        
+        return Integer.toBinaryString(temp);
+    }
+}
